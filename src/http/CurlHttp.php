8c1b1dfe9290de56414fd5f9fd2042196ac86407
Angel S. Moreno <angelxmoreno@gmail.com>
2014-05-10 00:13:51 -0400

    Added Curl Http

---
+++ src/http/CurlHttp.php
@@ -0,0 +1,13 @@
+<?php
+class CurlHttp implements InterfaceSimplyHiredJobAMaticApiHttp {
+	public function get($url){
+		$ch=curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		return $response;
+	}
+}
