e3b1b4fe8a37cd9d49ae30a933fed0869351d9ff
Angel S. Moreno <angelxmoreno@gmail.com>
2014-05-10 00:14:43 -0400

    broke response return into two lines for easier debuging

--- src/http/FileGetContentsHttp.php
+++ src/http/FileGetContentsHttp.php
@@ -1,6 +1,8 @@
 <?php
 class FileGetContentsHttp implements InterfaceSimplyHiredJobAMaticApiHttp {
 	public function get($url){
-		return file_get_contents($url);
+		$response =  file_get_contents($url);
+
+		return $response;
 	}
 }
