c082015f8e38250f541023656a4dd181fe2c93e1
Angel S. Moreno <angelxmoreno@gmail.com>
2014-05-10 00:17:55 -0400

    Expanded on examples and use of CurlHttp

--- sample.php
+++ sample.php
@@ -9,19 +9,34 @@ require_once dirname(__FILE__) . '/src/SimplyHiredJobAMaticApi/SimplyHiredJobAMa
 
 //include one of the Http Classes
 require_once dirname(__FILE__) . '/src/http/FileGetContentsHttp.php';
+require_once dirname(__FILE__) . '/src/http/CurlHttp.php';
 
 //create the Http obj
-$http = new FileGetContentsHttp();
+$http1 = new FileGetContentsHttp();
 
 $publisher_id = '28086';
 $domain = 'cakephp-jobs.jobamatic.com';
 
-$JobAMaticApi = new SimplyHiredJobAMaticApi($publisher_id, $domain, $http);
-$jobResults = $JobAMaticApi
-	->setQuery('ebay store')
+$JobAMaticApi1 = new SimplyHiredJobAMaticApi($publisher_id, $domain, $http1);
+$jobResults1 = $JobAMaticApi1
+	->setQuery('Ruby OR Rails')
+	->setLocation('San Francisco')
+	->setMiles(15)
+	->setSortByLastSeenDate('desc')
+	->setWindowSize(50)
+	->setFragmentDescription(false)
+	->request();
+print_r($jobResults1->toArray());
+/*********************************************************************************/
+$http2 = new CurlHttp();
+
+$JobAMaticApi2 = new SimplyHiredJobAMaticApi($publisher_id, $domain, $http1);
+$jobResults2 = $JobAMaticApi2
+	->setQuery('Wordpress')
 	->setLocation('10001')
-	->setMiles(1)
+	->setLocationExact()
 	->setSortByLastSeenDate('desc')
 	->setWindowSize(100)
+	->setFragmentDescription(true)
 	->request();
-var_dump($jobResults->toArray());
+print_r($jobResults2->toArray());
