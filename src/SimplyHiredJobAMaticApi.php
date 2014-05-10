2d0fb2b8ac9c84d6b050eb983d5c0305c2d1887c
Angel S. Moreno <angelxmoreno@gmail.com>
2014-05-10 01:17:43 -0400

    added doc blocks for the SimplyHiredJobAMaticApi class

--- src/SimplyHiredJobAMaticApi.php
+++ src/SimplyHiredJobAMaticApi.php
@@ -1,16 +1,73 @@
 <?php
 
+/**
+ * SimplyHiredJobAMaticApi class
+ * 
+ * A PHP wrapper for SimplyHired's Job-a-matic API
+ */
 class SimplyHiredJobAMaticApi {
 
+    /**
+     * The Publisher ID
+     * 
+     * @var string 
+     */
     protected $publisher_id;
+
+    /**
+     * The Domain of the Job-a-matic API
+     * 
+     * @var string
+     */
     protected $domain;
+
+    /**
+     *  The Object in charge of fetching the XML response from the API endpoint
+     * 
+     * @var InterfaceSimplyHiredJobAMaticApiHttp 
+     */
     protected $http;
 
+    /**
+     * Search Style
+     * A required search configuration value
+     * 
+     * @var string 
+     */
     protected $search_style = '2';
+
+    /**
+     * Configuration Flag
+     * A required search configuration value
+     * 
+     * @var string 
+     */
     protected $configuration_flag = 'r';
+
+    /**
+     * Client IP
+     * A required value, which contains the IP address of the visitor
+     * 
+     * @var string
+     */
     protected $client_ip;
+
+    /**
+     * Description Fragment
+     * An optional value, which is used to indicate if you choose to show
+     * clipped or full job description. By default, you will get clipped job
+     * description, unless the value is set to false or 0.
+     * 
+     * @var string 
+     */
     protected $description_fragment = '0';
 
+    /**
+     * A lookup array for mapping search variables to their corresponding
+     * variable names according to the API
+     * 
+     * @var array 
+     */
     protected $search_settings_map = array(
         'query' => 'q',
         'location' => 'l',
@@ -21,35 +78,62 @@ class SimplyHiredJobAMaticApi {
         'job_source_type' => 'fsr',
     );
 
+    /**
+     * The array that holds the search parameters and options set
+     * 
+     * @var string
+     */
     public $search_settings = array();
+
+    /**
+     * An array holding the available search options
+     * 
+     * @var array 
+     */
     protected $sort_options = array(
-        'rd',// relevance descending (default)
-        'ra',// relevance ascending
-        'dd',// last seen date descending
-        'da',// last seen date ascending
-        'td',// title descending
-        'ta',// title ascending
-        'cd',// company descending
-        'ca',// company ascending
-        'ld',// location descending
-        'la',// location ascending
+        'rd', // relevance descending (default)
+        'ra', // relevance ascending
+        'dd', // last seen date descending
+        'da', // last seen date ascending
+        'td', // title descending
+        'ta', // title ascending
+        'cd', // company descending
+        'ca', // company ascending
+        'ld', // location descending
+        'la', // location ascending
     );
+
+    /**
+     * An array holding the available job source types
+     * @var array
+     */
     protected $job_source_type_options = array(
-        'primary',// exclude job boards
-        'job_board',// job boards only
+        'primary', // exclude job boards
+        'job_board', // job boards only
     );
 
-
+    /**
+     * The endpoint for the API calls
+     * @var string
+     */
     protected $end_point = 'http://api.simplyhired.com/a/jobs-api/xml-v2';
 
-    function __construct($publisher_id, $domain, InterfaceSimplyHiredJobAMaticApiHttp $http) {
-        if(trim($publisher_id) == '' || is_null($publisher_id)){
+    /**
+     * Constructor
+     * 
+     * @param string $publisher_id
+     * @param string $domain
+     * @param InterfaceSimplyHiredJobAMaticApiHttp $http
+     * @throws Exception
+     */
+    public function __construct($publisher_id, $domain, InterfaceSimplyHiredJobAMaticApiHttp $http) {
+        if (trim($publisher_id) == '' || is_null($publisher_id)) {
             throw new Exception('The Publisher ID is required');
         } else {
             $this->publisher_id = $publisher_id;
         }
 
-        if(trim($domain) == '' || is_null($domain)){
+        if (trim($domain) == '' || is_null($domain)) {
             throw new Exception('The Job-a-matic Domain is required');
         } else {
             $this->domain = $domain;
@@ -57,100 +141,248 @@ class SimplyHiredJobAMaticApi {
         $this->http = $http;
     }
 
-    public function setQuery($query){
+    /**
+     * Sets the search query
+     * 
+     * Examples search criterias:
+     *      AND - Match all of the terms connected by AND. The default connector for search terms (case sensitive)
+     *      OR - Match at least one of the terms connected by OR (case sensitive)
+     *      NOT - Exclude matches on these terms (case sensitive)
+     *      ( ) - Group order of operations
+     * 
+     * The XML API supports basic Boolean searches as follows. The XML API also
+     * supports these job-related search requests:
+     *      All the words: Engineering AND Manager
+     *      Exact phrase: "Engineering Manager"
+     *      At least one of the words: Engineering OR Manager
+     *      Without words: Engineering NOT Manager
+     *      Job title: title:(Engineering Manager)
+     *      Company name: company:(Apple)
+     *      Occupation onet: onet:(13-205*) - Based on O*net, the U.S. Government's Occupation Classification System
+     * 
+     * @param string $query
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setQuery($query) {
         $this->search_settings['query'] = $query;
         return $this;
     }
 
-    public function setLocation($location){
+    /**
+     * Sets the location
+     * Location can be a zipcode, state, or city-state combination. Currently,
+     * there is no support for multiple location search.
+     * 
+     * @param string $location
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setLocation($location) {
         $this->search_settings['location'] = $location;
         return $this;
     }
 
-    public function setLocationExact(){
+    /**
+     * Sets miles to "mi-exact" for jobs only within a specific city
+     * 
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setLocationExact() {
         $this->search_settings['miles'] = 'mi-exact';
         return $this;
     }
 
-    public function setMiles($miles){
-        $miles = ((int)$miles <= 100 && (int)$miles >= 1) ? (int)$miles : 25;
+    /**
+     * Sets the miles parameter indicating the number of miles from the
+     * location.
+     * Miles value should be a number from "1" to "100". Miles represents the
+     * radius from the zip code, if specified in Location, or an approximate
+     * geographical "city center" if only city and state are present. If Miles
+     * is not specified, search will default to a radius of 25 miles.
+     * 
+     * @param integer $miles
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setMiles($miles) {
+        $miles = ((int) $miles <= 100 && (int) $miles >= 1) ? (int) $miles : 25;
         $this->search_settings['miles'] = $miles;
         return $this;
     }
 
-    public function setSort($sort){
+    /**
+     * Sets the sort the sort order of organic jobs (sponsored jobs have a
+     * fixed sort order)
+     * Valid values include:
+     *      rd = relevance descending (default)
+     *      ra = relevance ascending
+     *      dd = last seen date descending
+     *      da = last seen date ascending
+     *      td = title descending
+     *      ta = title ascending
+     *      cd = company descending
+     *      ca = company ascending
+     *      ld = location descending
+     *      la = location ascending
+     *
+     * 
+     * @param string $sort
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSort($sort) {
         $sort = in_array($sort, $this->sort_options) ? $sort : 'rd';
         $this->search_settings['sort'] = $sort;
         return $this;
     }
 
-    public function setSortByRelevance($direction = 'desc'){
+    /**
+     * Helper method for setting the sort parameter by revalance
+     * 
+     * @see setSort()
+     * @param string $direction
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSortByRelevance($direction = 'desc') {
         $sort = ($direction == 'asc') ? 'ra' : 'rd';
         $this->setSort($sort);
         return $this;
     }
 
-    public function setSortByLastSeenDate($direction = 'desc'){
+    /**
+     * Helper method for setting the sort parameter by last seen date
+     * 
+     * @see setSort()
+     * @param string $direction
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSortByLastSeenDate($direction = 'desc') {
         $sort = ($direction == 'asc') ? 'da' : 'dd';
         $this->setSort($sort);
         return $this;
     }
 
-    public function setSortByTitle ($direction = 'desc'){
+    /**
+     * Helper method for setting the sort parameter by title
+     * 
+     * @see setSort()
+     * @param string $direction
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSortByTitle($direction = 'desc') {
         $sort = ($direction == 'asc') ? 'ta' : 'td';
         $this->setSort($sort);
         return $this;
     }
 
-    public function setSortByCompany($direction = 'desc'){
+    /**
+     * Helper method for setting the sort parameter by company
+     * 
+     * @see setSort()
+     * @param string $direction
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSortByCompany($direction = 'desc') {
         $sort = ($direction == 'asc') ? 'ca' : 'cd';
         $this->setSort($sort);
         return $this;
     }
 
-    public function setSortByLocation($direction = 'desc'){
+    /**
+     * Helper method for setting the sort parameter by location
+     * 
+     * @see setSort()
+     * @param string $direction
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setSortByLocation($direction = 'desc') {
         $sort = ($direction == 'asc') ? 'la' : 'ld';
         $this->setSort($sort);
         return $this;
     }
 
-    public function setWindowSize($window_size){
-        $window_size = ((int)$window_size <= 100 && (int)$window_size >= 1) ? (int)$window_size : 10;
+    /**
+     * Sets the Window Size
+     * An integer representing the number of results returned. When available,
+     * the API will return 10 jobs by default. The API is limited to a maximum
+     * of 100 results per request.
+     * 
+     * @param integer $window_size
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setWindowSize($window_size) {
+        $window_size = ((int) $window_size <= 100 && (int) $window_size >= 1) ? (int) $window_size : 10;
         $this->search_settings['window_size'] = $window_size;
         return $this;
     }
 
-    public function setPageNumber($page_number){
-        $this->search_settings['page_number'] = (int)$page_number;
+    /**
+     * Sets the Page Number
+     * An integer representing the page number of the results returned.
+     * 
+     * @param integer $page_number
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setPageNumber($page_number) {
+        $this->search_settings['page_number'] = (int) $page_number;
         return $this;
     }
 
-    public function setJobSourceType($job_source_type){
+    /**
+     * Sets the Job Source
+     * A parameter indicating the job source type.
+     * Valid values include:
+     *      primary = exclude job boards
+     *      job_board = job boards only
+     * 
+     * @param string $job_source_type
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setJobSourceType($job_source_type) {
         $job_source_type = in_array($job_source_type, $this->job_source_type_options) ? $job_source_type : null;
         $this->search_settings['job_source_type'] = $job_source_type;
         return $this;
     }
 
-    public function setFragmentDescription($clip_description = 0){
-        $this->description_fragment = (int)$clip_description;
+    /**
+     * Sets the Description Fragment
+     * An optional value, which is used to indicate if you choose to show
+     * clipped or full job description. By default, you will get clipped job
+     * description.
+     * 
+     * @param boolean $clip_description
+     * @return \SimplyHiredJobAMaticApi
+     */
+    public function setFragmentDescription($clip_description = 0) {
+        $this->description_fragment = (int) $clip_description;
         return $this;
     }
 
-    public function request(){
+    /**
+     * Performs the request
+     * builds the endpoint for the given class search settings and uses the
+     * http object to perform a GET on the url
+     * 
+     * @return \SimplyHiredJobAMaticApi_Results
+     * @throws Exception
+     */
+    public function request() {
         $url = $this->_buildUrl();
         $raw_response = $this->http->get($url);
         $response = $this->_parseXmlResults($raw_response);
 
-        if($response instanceof SimplyHiredJobAMaticApi_Results) {
+        if ($response instanceof SimplyHiredJobAMaticApi_Results) {
             return $response;
-        } elseif(is_array($response)){
-            throw new Exception('API Error code: "' . $response['code'] . '" with message:"'. $response['type'].'"');
+        } elseif (is_array($response)) {
+            throw new Exception('API Error code: "' . $response['code'] . '" with message:"' . $response['type'] . '"');
         } else {
             throw new Exception('XML Error');
         }
     }
 
-    public function toArray(){
+    /**
+     * Helper method for converting this object to an array
+     * 
+     * @return array
+     */
+    public function toArray() {
         $properties = array(
             'publisher_id' => $this->publisher_id,
             'domain' => $this->domain,
@@ -161,6 +393,12 @@ class SimplyHiredJobAMaticApi {
         return array_merge($this->search_settings, $properties);
     }
 
+    /**
+     * Builds an endpoint using the given search settings
+     * 
+     * @param array $params
+     * @return string
+     */
     protected function _buildUrl(array $params = array()) {
         $get_params = array(
             'pshid' => $this->publisher_id,
@@ -174,6 +412,12 @@ class SimplyHiredJobAMaticApi {
         return $url;
     }
 
+    /**
+     * Builds the SimplyHiredJobAMaticApi_Results object using the XML response
+     * 
+     * @param string $xml_string
+     * @return \SimplyHiredJobAMaticApi_Results
+     */
     protected function _parseXmlResults($xml_string) {
         $xml = simplexml_load_string($xml_string);
         //check for errors
@@ -185,21 +429,32 @@ class SimplyHiredJobAMaticApi {
         return $resultsObj = new SimplyHiredJobAMaticApi_Results($xml, $this);
     }
 
+    /**
+     * Helper method for getting the remote ip and allowing the class to work
+     * via the commandline
+     * 
+     * @return string
+     */
     protected function _getRemoteIp() {
-        if(!$this->client_ip && (php_sapi_name() == 'cli' || !isset($_SERVER['REMOTE_ADDR'])) ) {
+        if (!$this->client_ip && (php_sapi_name() == 'cli' || !isset($_SERVER['REMOTE_ADDR']))) {
             $host = gethostname();
             $this->client_ip = gethostbyname($host);
-        } elseif(!$this->client_ip && isset($_SERVER['REMOTE_ADDR'])) {
+        } elseif (!$this->client_ip && isset($_SERVER['REMOTE_ADDR'])) {
             $this->client_ip = $_SERVER['REMOTE_ADDR'];
         }
         return $this->client_ip;
     }
-    
-    protected function _buildSearchPath(){
+
+    /**
+     * Converts the search settings into a path as per the API's documentation
+     * 
+     * @return string
+     */
+    protected function _buildSearchPath() {
         $path = '/';
-        foreach($this->search_settings_map as $search_property => $param){
+        foreach ($this->search_settings_map as $search_property => $param) {
             $val = (array_key_exists($search_property, $this->search_settings)) ? $this->search_settings[$search_property] : null;
-            if(!is_null($val)){
+            if (!is_null($val)) {
                 $path .= $param . '-' . urlencode($val) . '/';
             }
         }
